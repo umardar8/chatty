eae76f9301f5d03188248c6cf8d45ce75dcfce8a
umardar8 <umerfarooqdar.official@gmail.com>
2024-11-17 02:49:38 +0500

    location based messaging completed... maybe  *winks*

--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -12,15 +12,12 @@ import {
   DialogContent,
 } from "@/components/ui/dialog";
 import Mapbox from "../mapbox";
+import { toast } from "sonner";
 
 const MessageContainer = () => {
   const scrollRef = useRef(null);
-  const currentDate = moment(new Date()).format("LL");
-  const currentTime = moment(new Date()).format("LT");
-  const [receivedDate, setReceivedDate] = useState("");
-  const [receivedTime, setReceivedTime] = useState("");
-  const [expiryDate, setExpiryDate] = useState("");
-  const [expiryTime, setExpiryTime] = useState("");
+  const [currentDate, setCurrentDate] = useState(moment(new Date()).format("ll"));
+  const [currentTime, setCurrentTime] = useState(moment(new Date()).format("LT"));
   const [showLocation, setShowLocation] = useState(false);
   const [currentLocation, setCurrentLocation] = useState({
     currentLatitude: null,
@@ -33,6 +30,34 @@ const MessageContainer = () => {
     selectedChatMessages,
     setSelectedChatMessages,
   } = useAppStore();
+
+  const getMessages = async () => {
+      try {
+        const response = await apiClient.post(
+          GET_ALL_MESSAGES_ROUTE,
+          { id: selectedChatData._id },
+          { withCredentials: true }
+        );
+  
+        if (response.data.messages) {
+          setSelectedChatMessages(response.data.messages)
+        }
+
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        //Update currentDate and currentTime every minute
+        const now = new Date();
+        setCurrentDate(moment(now).format("ll"));
+        setCurrentTime(moment(now).format("LT"));
+      }, 1000); // Update every second
+    
+      return () => clearInterval(interval); // Cleanup on unmount
+    }, []);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -57,22 +82,6 @@ const MessageContainer = () => {
 
   useEffect(() => {
 
-    const getMessages = async () => {
-      try {
-        const response = await apiClient.post(
-          GET_ALL_MESSAGES_ROUTE,
-          { id: selectedChatData._id },
-          { withCredentials: true }
-        );
-  
-        if (response.data.messages) {
-          setSelectedChatMessages(response.data.messages) 
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
     if (selectedChatData._id && selectedChatType === "contact") getMessages();
   }, [selectedChatData, selectedChatType, setSelectedChatMessages]);
 
@@ -80,74 +89,7 @@ const MessageContainer = () => {
     if (scrollRef.current) {
       scrollRef.current.scrollIntoView({ behavior: "smooth" });
     }
-  }, [selectedChatMessages]);
-
-  // useEffect(() => {
-
-  //   return (
-
-  //   )
-
-  // }, [])
-
-  const renderLocationMessages = (message) => {
-    const receivedDate = moment(message?.startDate).format("ll");
-    const receivedTime = moment(message?.startTime, "HH:mm").format("h:mm A");
-    const expiryDate = moment(message?.endDate).format("ll");
-    const expiryTime = moment(message?.endTime, "HH:mm").format("h:mm A");
-  
-    const isTimeToDeliver = currentDate > receivedDate || (currentDate >= receivedDate && currentTime >= receivedTime)
-
-    console.log(`receivedDate: ${receivedDate}`)
-    console.log(`receivedTime: ${receivedTime}`)
-    console.log(`expiryDate: ${expiryDate}`)
-    console.log(`expiryTime: ${expiryTime}`)
-    console.log(`currentDate: ${currentDate}`)
-    console.log(`currentTime: ${currentTime}`)
-    console.log(`isTimeToDeliver: ${isTimeToDeliver}`)
-
-    return (
-      <>
-        {message.messageType === "text" && !message.location ? (
-          message.content
-        ) : isTimeToDeliver ? (
-          <>{message.content}</>
-        ) : (
-          <>
-            <p>
-              You have a new message at{" "}
-              <span
-                className="underline cursor-pointer"
-                onClick={() => setShowLocation(true)}
-              >
-                {message?.location?.location}
-              </span>
-            </p>
-          </>
-        )
-        <div
-          className={`
-            ${message.sender === selectedChatData._id ? "items-start" : "items-end"}
-            pt-3 flex flex-col text-[#808080] gap-2 text-xs hover:cursor-pointer hover:underline
-          `}
-        >
-          {isTimeToDeliver ? <span className="flex gap-2 items-center" onClick={() => setShowLocation(true)}>
-            {message?.location?.location} <FaLocationPinLock />
-          </span> : null}
-          <div className="flex gap-4 text-[#808080] text-xs">
-            <span className="flex gap-2 items-center">
-              {`${receivedDate} ${receivedTime}`} <FaClock className="text-green-500" />
-            </span>
-            <span className="flex gap-2 items-center">
-              {`${expiryDate} ${expiryTime}`} <FaClock className="text-red-400" />
-            </span>
-          </div>
-        </div>
-        }
-      </>
-    );
-  };
-  
+  }, [selectedChatMessages]);  
 
   const renderMessages = () => {
     let lastDate = null;
@@ -168,7 +110,16 @@ const MessageContainer = () => {
     });
   };
 
-  const renderDMMessages = (message) => (
+  const renderDMMessages = (message) => {
+
+    const receivedDate = moment(message?.startDate).format("ll");
+    const receivedTime = moment(message?.startTime, "HH:mm").format("h:mm a");
+    const expiryDate = moment(message?.endDate).format("ll");
+    const expiryTime = moment(message?.endTime, "HH:mm").format("h:mm a");
+  
+    const isTimeToDeliver = currentDate > receivedDate || (currentDate == receivedDate && currentTime >= receivedTime)
+    
+    return (
     <div className={`${ message.sender === selectedChatData._id ? "text-left" : "text-right" }`}>
 
       <div
@@ -177,84 +128,51 @@ const MessageContainer = () => {
             message.sender !== selectedChatData._id
               ? "bg-[#00ccff]/5 text-[#00ccff]/90 border-[#00ccff]/50"
               : "bg-[#2a2b33]/5 text-white/80 border-[#ffffff]/20"
-          } border inline-block p-4 rounded my-1 max-w-[75%] break-words
+          } border inline-block p-4 rounded my-1 max-w-[66%] break-words
         `}
       >
-        {renderLocationMessages(message)}
-
-        {/* {message.messageType === "text" && message.location === undefined 
-        ? (message.content) 
-        : currentDate >= moment(message.startDate).format("LL") &&
-        currentTime >= moment(message?.startTime,"HH:mm").format("h:mm A")
-          ? (
-          <>
-            {message.content}
-            <div
-              className={`
-                ${ message.sender === selectedChatData._id ? "justify-start" : "justify-end" } 
-                pt-2 flex text-[#808080] gap-2 text-xs items-center hover:cursor-pointer hover:underline
-              `}
+        <>
+        {message.messageType === "text" && !message.location ? (
+          message.content
+        ) : (<>
+          {isTimeToDeliver 
+          ? <>{message.content}</>
+          : (
+              <>
+                You have a new message at{" "}
+                <span
+                  className="underline cursor-pointer"
+                  onClick={() => setShowLocation(true)}
+                >
+                  {message?.location?.location}
+                </span>
+              </>
+          )}
+        <div
+          className={`
+            ${message.sender === selectedChatData._id ? "items-start" : "items-end"}
+            pt-3 flex flex-col text-[#808080] gap-2 text-xs
+          `}
+        >
+          
+          {isTimeToDeliver ? 
+            <span 
+              className="flex gap-2 items-center hover:cursor-pointer hover:underline" 
               onClick={() => setShowLocation(true)}
             >
-              {message?.location?.location} <FaLocationPinLock />
-            </div>
-            <div
-              className={`
-                ${
-                  message.sender === selectedChatData._id ? "justify-start" : "justify-end"
-                } flex gap-4 text-[#808080] text-xs mt-2
-              `}
-            >
-              <span className="flex gap-2 items-center">
-                {`
-                    ${moment(message?.startDate).format("ll")} 
-                    ${moment(message?.startTime, "HH:mm").format("h:mm A")}
-                  `}
-                  <FaClock className="text-green-500" />
-              </span>
-              <span className="flex gap-2 items-center">
-                {`
-                    ${moment(message?.endDate).format("ll")}  
-                    ${moment(message?.endTime, "HH:mm").format("h:mm A")}
-                  `}
-                  <FaClock className="text-red-400" />
-              </span>
-            </div>
-          </>
-        ) : (
-          <div className="flex flex-col">
-            <p>
-              You have a new message at{" "}
-              <span
-                className="underline cursor-pointer"
-                onClick={() => setShowLocation(true)}
-              >
-                {message?.location?.location}
-              </span>
-            </p>
-            <div
-              className={`
-                ${ message.sender === selectedChatData._id ? "justify-start" : "justify-end" } 
-                flex gap-4 text-[#808080] text-xs mt-2
-              `}
-            >
-              <span className="flex gap-2 items-center">
-                <FaClock className="text-green-500" />
-                {`
-                    ${moment(message?.startDate).format("ll")} 
-                    ${moment(message?.startTime, "HH:mm").format("h:mm A")}
-                `}
-              </span>
-              <span className="flex gap-2 items-center">
-                <FaClock className="text-red-400" />
-                {`
-                    ${moment(message?.endDate).format("ll")}  
-                    ${moment(message?.endTime, "HH:mm").format("h:mm A")}
-                `}
-              </span>
-            </div>
+            {message?.location?.location} <FaLocationPinLock />
+          </span> : null}
+          <div className="flex gap-4 text-[#808080] text-xs">
+            <span className="flex gap-2 items-center">
+              {`${receivedDate} ${receivedTime}`} <FaClock className="text-green-500" />
+            </span>
+            <span className="flex gap-2 items-center">
+              {`${expiryDate} ${expiryTime}`} <FaClock className="text-red-400" />
+            </span>
           </div>
-        )} */}
+        </div>
+        </>)}
+      </>
       </div>
       <div className="text-xs text-gray-600 ">
         {moment(message.timestamp).format("LT")}
@@ -271,7 +189,7 @@ const MessageContainer = () => {
         </DialogContent>
       </Dialog>
     </div>
-  );
+  )}
   return (
     <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full">
       {renderMessages()}
