a66bdcae477f5e69c430e53263e1e1f0092b01f6
Umar <umerfarooqdar.official@gmail.com>
2025-01-20 05:08:05 +0500

    moving all the project code of localhost environment to a separate branch for isolation from the main branch for vercel

--- client/src/lib/api-client.js
+++ client/src/lib/api-client.js
@@ -2,5 +2,5 @@ import axios from "axios";
 import { HOST } from "@/utils/constants.js";
 
 export const apiClient = axios.create({
-    baseURL: "https://chatty-server-cyan.vercel.app",
+    baseURL: HOST,
 })
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const databaseURL = process.env.DATABASE_URL; // uri of database on mongoDB Atla
 // using cors to enable interaction of multiple servers
 app.use(
     cors({
-        origin: ["https://chatty-client-tau.vercel.app"],
+        origin: process.env.ORIGIN,
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true
     })
