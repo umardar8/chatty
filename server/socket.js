6acad6b00ed81c22e57fa02becc29db66601ccfc
Umar <umerfarooqdar.official@gmail.com>
2025-01-22 23:45:51 +0500

    making more more improvements

--- server/socket.js
+++ server/socket.js
@@ -5,7 +5,7 @@ const setupSocket = (server) => {
 
     const io = new SocketIoServer(server, {
         cors: {
-            origin: 'https://chatty-muet.vercel.app',
+            origin: 'https://chatty-muet-app.vercel.app',
             methods: ["GET", "POST"],
             credentials: true
         }
