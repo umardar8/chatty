8722cf6da6b51c3403f2f426217bac4db3c55419
Umar <umerfarooqdar.official@gmail.com>
2025-01-22 22:10:47 +0500

    resolving more errors

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -8,7 +8,6 @@ authRoutes.post("/signup", signup);
 authRoutes.post("/login", login);
 authRoutes.get("/user-info", verifyToken, getUserInfo);
 authRoutes.post("/update-profile", verifyToken, updateProfile);
-authRoutes.post("/add-profile-image", verifyToken, upload.single("profile-image"), addProfileImage);
 authRoutes.delete("/remove-profile-image", verifyToken, removeProfileImage)
 authRoutes.post("/logout", logout)
 
