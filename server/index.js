5630daf6deb56806defa0486efca89bf68bf64eb
Umar <umerfarooqdar.official@gmail.com>
2025-01-22 22:46:31 +0500

    trying to resolve the CORS error

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const databaseURL = process.env.DATABASE_URL;
 // using cors to enable interaction of multiple servers
 app.use(
     cors({
-        origin: process.env.ORIGIN,
+        origin: '*',
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true
     })
